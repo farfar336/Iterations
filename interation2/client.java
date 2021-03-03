fc4669b55bedfa94a010bc43c366f0e331c134a3
miaoxudong111 <xudong.miao1@ucalgary.ca>
2021-03-02 17:24:39 -0700

    Update client.java

--- interation2/client.java
+++ interation2/client.java
@@ -80,6 +80,9 @@ public class client{
 	public static Executor executor,executor1,executor2;
 	public static final int inactiveTimeLimit=240;
 	public static final int maxSnipLength=25;
+	// this is a hashmap that stores location and time
+	//location is the other peer's location
+	//time is the timestamp(in second) that receive the peer info from this peer's location
 	public static ConcurrentHashMap<String,Long> locationAndTime;
 	
 	// Sends the string to the server
@@ -386,35 +389,21 @@ public class client{
 		executor2 = Executors.newSingleThreadExecutor();
 		nextSnipTimestamp = 0;
 	}
-	
-	// To do: Comment on what this function does 
-	public static void sendPeer() throws InterruptedException {
-		if(currentPeerList.size()>0) {
-			for(int i = 0; i < currentPeerList.size(); i++) {
-				InetAddress ip;
-				System.out.println(currentPeerList.get(i));
-				try {
-					ip = InetAddress.getByName(currentPeerList.get(i).split(":")[0].toString().replace("/", ""));
-					int port=Integer.parseInt(currentPeerList.get(i).split(":")[1]);
-					String message="peer"+peer.getAddress()+":"+peer.getPort();
-					peer.sendMessage(message.replace("/", ""), ip, port);
-				} catch (UnknownHostException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}				
-			}
-		}			
-	}
 
-	// To do: Comment on what this function does 
+
+	// Detect if the time difference between current time and recorded time is greater than 240 seconds
+	//return true if time difference is greater than 240 seconds
+	//otherwise return false
 	public static Boolean notResponse(long currentTime,long time) {
 		return(currentTime-time)>inactiveTimeLimit;
 	}
 	
-	// To do: Comment on what this function does 
+	// peer.getMessage() will return a peer's location, if any other peer send their peer info. return null if it is not peer info
+	// update the snips with the latest snips in the peer class
+	// 
+	//if any peer send peer information, newPeer= this peer's location otherwise newPeer=null
+	//update the time of location in the hashmap
+
 	public static void receiveMessage() {
 		try {
 			String newPeer = peer.getMessage();
@@ -436,7 +425,7 @@ public class client{
 		}
 	}
 
-	// To do: Comment on what this function does 
+	// a thread collaborate with other peers, send peer information to other peers
 	static Thread collaborationThread = new Thread (new Runnable() {
 		public void run() {
 			while(!peer.stop) {
@@ -451,7 +440,11 @@ public class client{
 		}	
 	});
 
-	// To do: Comment on what this function does 
+	//get input from user and modify the input, if input has more than 25 characters, get the first 25 character
+	//snip = timestamp+input+address of this peer:port of this peer
+	//add snip to snips
+	//send snips
+
 	static Thread getSnipThread = new Thread(new Runnable() {
 		@Override
 		public void run() {
@@ -479,7 +472,8 @@ public class client{
 		}
 	});
 	
-	// To do: Comment on what this function does
+	// check if a peer is active or not. If a peer does not send their peer info for more than 4 mins
+	// ask peer to delete this inactive peer from activePeerList
 	static Thread checkActivePeerThread=new Thread(new Runnable() {
 		@Override
 		public void run() {
@@ -489,7 +483,6 @@ public class client{
 				    String location = entry.getKey();
 				    long time = entry.getValue();
 				    if(notResponse(currentTime,time)) {
-				    	//did not update after deleting
 				    	System.out.println(location + "    disconnected" );
 				    	peer.removePeerFromActivePeerList(location);
 				    	locationAndTime.remove(location);
